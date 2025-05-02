ce5b7df288329a666a91121f099e742981652c9e
lavrentious <lavrent0@yandex.ru>
2025-05-02 19:21:02 +0300

    fix: disable submit if invalid community cards count

--- client/src/modules/poker/pages/MainPage/SubmitSimulationButton.tsx
+++ client/src/modules/poker/pages/MainPage/SubmitSimulationButton.tsx
@@ -6,9 +6,15 @@ import { RootState } from "src/store";
 const SubmitSimulationButton = () => {
   const holeCard0 = useSelector((state: RootState) => state.poker.holeCards[0]);
   const holeCard1 = useSelector((state: RootState) => state.poker.holeCards[1]);
+  const communityCards = useSelector(
+    (state: RootState) => state.poker.communityCards,
+  );
   const submitActive = useMemo(() => {
-    return holeCard0 && holeCard1;
-  }, [holeCard0, holeCard1]);
+    if (!holeCard0 || !holeCard1) return false;
+    const activeCommunityCards = communityCards.filter((e) => !!e).length;
+    if (0 < activeCommunityCards && activeCommunityCards < 3) return false;
+    return true;
+  }, [holeCard0, holeCard1, communityCards]);
 
   return (
     <Button type="submit" variant="primary" disabled={!submitActive}>
