fb7220a459a75a7bd1d4d16c73243db02ef323f2
lavrentious <lavrent0@yandex.ru>
2025-05-03 22:50:40 +0300

    fix(client): CardValue to int

--- client/src/modules/poker/services/api/utils.ts
+++ client/src/modules/poker/services/api/utils.ts
@@ -12,19 +12,32 @@ export function cardValueToInt(value: CardValue): CardValueInt {
     case CardValue.JACK:
       return 11;
     case CardValue.TEN:
+      return 10;
     case CardValue.NINE:
+      return 9;
     case CardValue.EIGHT:
+      return 8;
     case CardValue.SEVEN:
+      return 7;
     case CardValue.SIX:
+      return 6;
     case CardValue.FIVE:
+      return 5;
     case CardValue.FOUR:
+      return 4;
     case CardValue.THREE:
+      return 3;
     case CardValue.TWO:
-      return parseInt(value) as CardValueInt;
+      return 2;
   }
 }
 
 export function cardToApiCard(card: CardType): ApiCard {
+  console.log("converting to api card", card);
+  console.log({
+    suit: card.suit.toLowerCase() as ApiCard["suit"],
+    rank: cardValueToInt(card.value),
+  });
   return {
     suit: card.suit.toLowerCase() as ApiCard["suit"],
     rank: cardValueToInt(card.value),
