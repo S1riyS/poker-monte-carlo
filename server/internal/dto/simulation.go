a9e1b95c26f6aab1afc7875656ab83f88fcf93bd
Kirill Ankudinov <kirill.ankudinov.94@mail.ru>
2025-05-04 13:57:38 +0300

    📝 feat(dto): validation and docs for simulation DTOs

--- server/internal/dto/simulation.go
+++ server/internal/dto/simulation.go
@@ -4,29 +4,31 @@ import (
 	"github.com/S1riyS/poker-monte-carlo/pkg/poker"
 )
 
+// NOTE: see pkg/poker/constants.go to determin min and max for card rank
+// (min=MIN_CARD_RANK, max=MAX_CARD_RANK)
+type SimulationCard struct {
+	Rank int        `json:"rank" validate:"required,min=2,max=14" example:"11"`
+	Suit poker.Suit `json:"suit" validate:"required,poker_suit_enum" example:"diamonds"`
+}
+
 type SimulationRequest struct {
-	Iterations int
-	Players    int
-	Hand       []poker.Card
-	Table      []poker.Card
+	// Number of iterations in a simulation
+	Iterations int `json:"iterations" validate:"required,max=1000000" example:"10000"`
+	// Number of other players at the table
+	Players int `json:"players" validate:"required,min=1,max=10" example:"5"`
+	// Starting hand
+	Hand []SimulationCard `json:"hand" validate:"required,len=2"`
+	// Cards on the table (flop, turn & river)
+	Table []SimulationCard `json:"table" validate:"required,max=5"`
 }
 
 type CombinationResult struct {
-	Name string
-	Win  int
-	Lose int
-	Tie  int
+	Name string `json:"name" example:"straight"`
+	Win  int    `json:"win" example:"158"`
+	Lose int    `json:"lose" example:"33"`
+	Tie  int    `json:"tie" example:"7"`
 }
 
 type SimulationResponse struct {
-	Data []CombinationResult
-}
-
-func NewMockSimulationResponse() SimulationResponse {
-	return SimulationResponse{
-		Data: []CombinationResult{
-			{"high_card", 187, 200, 0},
-			{"straight", 9, 0, 5},
-		},
-	}
+	Data []CombinationResult `json:"data"`
 }
